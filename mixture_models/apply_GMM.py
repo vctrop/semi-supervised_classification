Fix log-likelihood and check if covariance matrix is singular (Future: timing of my implementation vs numpy cov and scipy pdf)

mixture_models/apply_GMM.py
@@ -19,25 +19,54 @@
 #import math                    # Single-value math operations
 import numpy as np              # Vector math operations
 import gaussian_mixture_model   #
-from sklearn.datasets import load_iris
+from sklearn.datasets import make_classification
+import matplotlib.pyplot as plt
 
-full_iris = load_iris()
+# # IRIS DATASET
+# full_iris = load_iris()
+# # Restrict data to two features (petal len, wid)
+# y_array = np.array(full_iris.target)
+# iris_data = full_iris.data [ np.logical_or(full_iris.target == 0, y_array == 1)]
+# # Restrict data to two classes (setosa, versicolor)
+# iris_labels = y_array [ np.logical_or(y_array == 0, y_array == 1) ]
+# iris_data = iris_data[:, 0:2]
 
-# Restrict data to two features (petal len, wid)
-y_array = np.array(full_iris.target)
-iris_labels = y_array [ np.logical_or(y_array == 0, y_array == 1) ]
-iris_data = full_iris.data [ np.logical_or(full_iris.target == 0, y_array == 1)]
-# Restrict data to two classes (setosa, versicolor)
-iris_data = iris_data[:, 0:2]
+# Synthetic Gaussian dataset
+X, y = make_classification(n_samples=100, n_features=2, n_redundant=0, n_classes=2, n_clusters_per_class=1, random_state=42)
+rng = np.random.RandomState(2)
+X += 3 * rng.uniform(size=X.shape)
+
+plt.figure()
+plt.scatter(X[:, 0], X[:, 1], marker='o', c=y,
+            s=25, edgecolor='k')
+plt.plot()
+plt.show()
+            
+            
+print('[Fully supervised]')
+weights_labeled = np.transpose(np.vstack( (y, (1-y)) ))
+_, all_means, all_covariances = gaussian_mixture_model.maximum_likelihood_estimation(X, weights_labeled, 2)
+print('f1 means: ' + str(all_means[0]))
+print('f2 means: ' + str(all_means[1]))
+print('f1 cov: \n' + str(all_covariances[0]))
+print('f2 cov: \n' + str(all_covariances[1]))
 
 # Split data in labeled and unlabeled
-labeled_data = iris_data[40:60]
-partial_labels = iris_labels[40:60]
-unlabeled_data = np.vstack((iris_data[:40], iris_data[60:]))
+labeled_data = X[45:55]
+partial_labels = y[45:55]
+unlabeled_data = np.vstack((X[:45], X[55:]))
 
 # print(np.shape(labeled_data))
 # print(np.shape(partial_labels))
 # print(np.shape(unlabeled_data))
 
-log_likelihood, _, _, _ = gaussian_mixture_model.expectation_maximization(labeled_data, unlabeled_data, partial_labels, 1)
-print(log_likelihood)
+iterations = [1, 100, 200]
+for i in iterations:
+    log_likelihood, _, all_means, all_covariances = gaussian_mixture_model.expectation_maximization(labeled_data, unlabeled_data, partial_labels, i)
+    print('\n[Semi-supervised ' + str(i) + ' iteration]')
+    print('f1 means: ' + str(all_means[0]))
+    print('f2 means: ' + str(all_means[1]))
+    print('f1 cov: \n' + str(all_covariances[0]))
+    print('f2 cov: \n' + str(all_covariances[1]))
+
+

mixture_models/gaussian_mixture_model.py
@@ -20,6 +20,7 @@ import math             # Single-value math operations
 import numpy as np      # Vector math operations
 from scipy.stats import multivariate_normal
 
+
 # Compute the probability of a given data array being sampled from a given multivariate Gaussian distribution
 def multivariate_gaussian_pdf(data_array, means_array, covariance_matrix):
     data_array = np.array(data_array)
@@ -32,9 +33,13 @@ def multivariate_gaussian_pdf(data_array, means_array, covariance_matrix):
     if len(covariance_matrix[:,0]) != len(covariance_matrix[0,:]) or len(covariance_matrix[:,0]) != len(data_array):
         print("Error, invalid dimensionality for covariance matrix")
         exit(-1)
-    
+        
     # Compute determinant and inverse of covariance matrix
     cov_det = np.linalg.det(covariance_matrix)
+    if cov_det == 0:
+        print('Error, covariance matrix is singular')
+        exit(-1)
+        
     cov_inv = np.linalg.pinv(covariance_matrix)
     
     # Compute exponent of Gaussian distribution
@@ -53,20 +58,20 @@ def multivariate_gaussian_pdf(data_array, means_array, covariance_matrix):
 def gaussian_mixture_log_likelihood(X_labeled, y_array, X_unlabeled, all_priors, all_means, all_covariances, num_classes):
     
     log_likelihood = 0.0
-    for j in range(num_classes):
-        class_prior = all_priors[j]
-        class_means_array = all_means[j]
-        class_cov_matrix = all_covariances[j]
-        X_class = X_labeled[y_array == j]
-        y_class = y_array[y_array == j]
+    # Labeled data likelihood
+    for data_array, y in zip(X_labeled, y_array):
+        class_conditional = multivariate_gaussian_pdf(data_array, all_means[y], all_covariances[y])            
+        # log_likelihood += math.log( all_priors[y] * class_conditional )
+        log_likelihood += all_priors[y] * class_conditional
+    # Unlabeled data likelihood, marginalizing over the classes
+    for data_array in X_unlabeled:
+        class_marginalized_likelihood = 0.0
+        for j in range(num_classes):
+            class_conditional = multivariate_gaussian_pdf(data_array, all_means[j], all_covariances[j])
+            class_marginalized_likelihood += all_priors[j] * class_conditional
         
-        for data_array, y in zip(X_class, y_class):
-            class_conditional = multivariate_gaussian_pdf(data_array, class_means_array, class_cov_matrix)            
-            log_likelihood += math.log( class_prior * class_conditional )
-        
-        for data_array in X_unlabeled:
-            class_conditional = multivariate_gaussian_pdf(data_array, class_means_array, class_cov_matrix)
-            log_likelihood += math.log ( class_prior  * class_conditional )
+        # log_likelihood += math.log ( class_marginalized_likelihood )
+        log_likelihood += class_marginalized_likelihood
         
     return log_likelihood
     
@@ -81,7 +86,6 @@ def maximum_likelihood_estimation(X, weights, num_classes):
         
     X = np.array(X)
     weights = np.array(weights)
-    
     # Store means arrays and covariance matrices for all classes
     all_priors = []
     all_means = []
@@ -89,8 +93,9 @@ def maximum_likelihood_estimation(X, weights, num_classes):
     
     # Compute distribution parameters for each class
     for j in range(num_classes):
+    
         class_len = np.sum(weights[:,j])
-        
+        # print(class_len)
         # Compute priors and means
         class_prior = class_len / len(weights)
         weighted_X = X * (weights[:, j])[:, None]
@@ -98,7 +103,7 @@ def maximum_likelihood_estimation(X, weights, num_classes):
         dimensionality = len(class_means_array)
         
         # Compute covariance matrix
-        transp_class_X = (X[weights[:,j]==1]).T
+        # transp_class_X = (X[weights[:,j]==1]).T
         # np_cov = np.cov(transp_class_X, bias=True)
         # print(np_cov)
         
@@ -111,8 +116,6 @@ def maximum_likelihood_estimation(X, weights, num_classes):
                 class_cov_matrix += submatrix
                 
         class_cov_matrix = class_cov_matrix / class_len  
-        # print(class_cov_matrix)
-
         all_priors.append(class_prior)
         all_means.append(class_means_array)
         all_covariances.append(class_cov_matrix)
@@ -130,41 +133,29 @@ def expectation_maximization(X_labeled, X_unlabeled, y, num_iterations):
     # Initialize MLE on labeled data
     # Priors are used for all instances
     all_priors, all_means, all_covariances = maximum_likelihood_estimation(X_labeled, weights_labeled, num_classes)
-    # print(all_priors)
-    # print(all_means)
-    # print(all_covariances)
     
     ## Loop
-    #for _ in range(num_iterations):
-    for _ in range(1):
+    for _ in range(num_iterations):
         # Assign conditional probabilities p(y|x) to unlabeled data with Gaussian mixture
         unlabeled_conditionals = []
         for data_array in X_unlabeled:
             # Compute 
             array_conditionals = []
             for j in range(num_classes):
-                class_prior = all_priors[j]
-                class_means_array = all_means[j]
-                class_cov_matrix = all_covariances[j]
-                
-                class_conditional_prob = multivariate_gaussian_pdf(data_array, class_means_array, class_cov_matrix)
-                print('my: ' + str(class_conditional_prob))
-                print('scipy: ' + str(multivariate_normal(class_means_array, class_cov_matrix).pdf(data_array)))
-                # exit(-1)
-                
-                # print(class_conditional_prob)
-                
-                array_conditionals.append(class_prior * class_conditional_prob)
-            array_conditionals = np.array(array_conditionals)/np.sum(array_conditionals)
-            # print(array_conditionals)
+                class_conditional_prob = multivariate_gaussian_pdf(data_array, all_means[j], all_covariances[j])
+                # print(multivariate_normal(all_means[j], all_covariances[j]).pdf(data_array))
+                array_conditionals.append(all_priors[j] * class_conditional_prob)
+            
+            array_conditionals = np.array(array_conditionals)/sum(array_conditionals)
             unlabeled_conditionals.append(array_conditionals)
         
         # Compute MLE on whole data
         all_weights = np.vstack((weights_labeled, unlabeled_conditionals))
         all_priors, all_means, all_covariances = maximum_likelihood_estimation(np.vstack((X_labeled,X_unlabeled)), all_weights, num_classes)
-    
+
         # Check for convergence
         mixture_log_likelihood = gaussian_mixture_log_likelihood(X_labeled, y, X_unlabeled, all_priors, all_means, all_covariances, num_classes)
+        # print(mixture_log_likelihood)
     
     return mixture_log_likelihood, all_priors, all_means, all_covariances
     